ce79400388bb3f9d2f183797cc7e21c8bbdac5dd
Andrew <alo42@georgetown.edu>
2020-10-21 16:33:23 -0400

    cleans deps

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR "/app"
 COPY package.json .
 RUN npm install
 COPY . .
-EXPOSE 3000
-RUN ["npm", "run", "start"]
+EXPOSE 4000
+CMD ["npm", "run", "start"]
--- src/index.js
+++ src/index.js
@@ -4,28 +4,25 @@ const resolvers = require('./graphql/resolvers')
 const neo4j = require('neo4j-driver')
 const keys = require('./keys')
 
-console.log(keys)
-
 const driver = neo4j.driver(
-    keys.neo4jUri,
-    neo4j.auth.basic(
-      keys.neo4jUser,
-      keys.neo4jPassword
-    ),
-    {
-      encrypted: keys.neo4jEncrypted ? 'ENCRYPTION_ON' : 'ENCRYPTION_OFF',
-    }
-  )
+  keys.neo4jUri,
+  neo4j.auth.basic(
+    keys.neo4jUser,
+    keys.neo4jPassword
+  ),
+  {
+    encrypted: keys.neo4jEncrypted ? 'ENCRYPTION_ON' : 'ENCRYPTION_OFF',
+  }
+)
 
-  const server = new ApolloServer({ 
-      typeDefs, 
-      resolvers,
-      context: { driver },
-      introspection: true,
-      playground: true
-    });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers,
+  context: { driver },
+  introspection: true,
+  playground: true
+});
 
-// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
