5a30f9f7694fb4582b6909546c5a9bc29dc6aa41
Andrew <alo42@georgetown.edu>
2020-11-09 17:53:51 -0800

    modifies babel script

--- api/build/src/scripts/mocks/seedDbData.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports.names = ["Andrew", "Jamie", "John", "Reginald", "Roger", "Jefferey"];
-module.exports.isVisible = [true, false];
-module.exports.bios = ["Student just chillin", "I am a narc", "Wannabe policeman", "Software engineer just hangin"];
-module.exports.whatAmIDoings = ["Reading War and Peace and waiting to be interrupted", "Doing this and that", "Drinking a coffee"];
-module.exports.sex = ["male", "female"];
-module.exports.ages = [24, 20, 32, 43, 18, 14];
-module.exports.emails = ["me1@g.com", "me2@g.com", "me3@g.com"];
