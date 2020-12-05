e7c371d6a02cf6079207e485637feb67bddca97d
Andrew <alo42@georgetown.edu>
2020-12-05 17:50:49 -0500

    updates media services

--- media-services/src/index.ts
+++ media-services/src/index.ts
@@ -2,12 +2,14 @@ import upload from './aws/upload';
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config({ path: path.resolve(__dirname, `../../.env.${process.env.NODE_ENV}`) });
 
 const app = express();
+app.use(cors())
 
-app.post('/upload', upload.single('photo'), (req, res) => {
-  res.json(req.file);
+app.post('/media/upload', upload.array('photo'), (req, res) => {
+  res.send("Uploaded!")
 });
 
 const port = process.env.PORT || 3000;
