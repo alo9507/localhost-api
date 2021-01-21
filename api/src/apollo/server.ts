96a0ceba73baa4c2da64cb49c4d50403ffa8af69
Andrew <alo42@georgetown.edu>
2021-01-20 20:32:53 -0500

    last commit before I integrate Firebase Cloud Messaging

--- api/src/apollo/server.ts
+++ api/src/apollo/server.ts
@@ -3,12 +3,13 @@ import typeDefs from '../graphql/schema/schema';
 import resolvers from '../graphql/resolvers/resolvers';
 import createDriver from '../neo4j/driver';
 import AnalyticsExtension from '../graphql/extensions/AnalyticsExtensions';
+import PushNotificationService from "../services/PushNotificationServices"
 
 function createServer(databaseUri) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: { driver: createDriver(databaseUri) },
+    context: { driver: createDriver(databaseUri), pushNotificationService: new PushNotificationService() },
     introspection: true,
     playground: true,
     extensions: [() => new AnalyticsExtension()]
---
+++ api/src/services/PushNotificationServices.ts
@@ -0,0 +1,5 @@
+class PushNotificationService {
+    constructor () { }
+}
+
+export default PushNotificationService
